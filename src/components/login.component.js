86b06def7e4e2d05575c5f4d16502cbc1981ffb9
BuiKimPhat <buikimphat42@gmail.com>
2020-01-24 17:20:48 +0700

    ip

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -31,7 +31,7 @@ export default class Login extends React.Component {
             password: this.state.password
         }
 
-        axios.post('http://localhost:6969/login', user)
+        axios.post('http://10.148.0.11:6969/login', user)
             .then(res => {
                 if (res.data === "User not found!") alert("Wrong username or password!");
                 else {
